ad7018c1eb8b741b113dc85152786799ebe67398
diegohenao819 <diegohenao819@gmail.com>
2024-12-31 15:56:21 -0500

    Tree

---
+++ tree/tree.js
@@ -0,0 +1,32 @@
+function calculatePrice(ornaments) {
+  const adornos = {
+    "*": 1,
+    "o": 5,
+    "^": 10,
+    "#": 50,
+    "@": 100,
+  };
+  
+  for (let element of ornaments) {
+    if (!adornos[element]) {
+      return undefined;
+    }
+  }
+
+  
+  let total = 0;
+  for (let i = 0; i < ornaments.length; i++) {
+    const a = adornos[ornaments[i]];
+    const b = adornos[ornaments[i + 1]] || 0;
+    if (a < b) {
+      total -= a;
+    } else {
+      total += a;
+    }
+  }
+
+  return total;
+}
+
+console.log(calculatePrice("*o@"));
+// 94
