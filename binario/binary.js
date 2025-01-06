97311aae99a04c60ffd6dc597947b4a6eaa40190
diegohenao819 <diegohenao819@gmail.com>
2025-01-06 17:07:58 -0500

    Add treeHeight function and update README with problem description and examples for binary tree height calculation

---
+++ binario/binary.js
@@ -0,0 +1,49 @@
+function treeHeight(tree) {
+  if (tree === null) {
+    return 0;
+  }
+
+  const left = treeHeight(tree.left);
+
+  const right = treeHeight(tree.right);
+
+  return Math.max(left, right) + 1;
+}
+
+// Definición del árbol
+const tree = {
+  value: "🎁",
+  left: {
+    value: "🎄",
+    left: {
+      value: "⭐",
+      left: null,
+      right: null,
+    },
+    right: {
+      value: "🎅",
+      left: null,
+      right: null,
+    },
+  },
+  right: {
+    value: "❄️",
+    left: null,
+    right: {
+      value: "🦌",
+      left: null,
+      right: null,
+    },
+  },
+};
+
+// Representación gráfica del árbol:
+//        🎁
+//       /   \
+//     🎄     ❄️
+//    /  \      \
+//  ⭐   🎅      🦌
+
+// Llamada a la función
+console.log(treeHeight(tree, 0));
+// Devuelve: 3
